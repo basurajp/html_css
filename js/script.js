54ed59d131d2a964f86902f7fd6ab7762b142a55
Basuraj <basurajp135@gmail.com>
2023-11-17 22:51:38 +0530

    timer  using js  done

--- js/script.js
+++ js/script.js
@@ -1,61 +1,20 @@
-// var btn = document.querySelector("button");
-// var h = document.querySelector("h1");
-
-// btn.addEventListener("click", function () {
-//   h.textContent = "Hello World!";
-// });
-
-// var img1 = document.querySelector("#img1");
-// var img2 = document.querySelector("#img2");
-
-// var btn = document.querySelector("button");
-
-// btn.addEventListener("click", function () {
-//     var img1scr = img1.src
-//     var img2scr = img2.src
-
-//     img1.src = img2scr
-//     img2.src = img1scr
-//     console.log('basuraj');
-// })
-
-// var inputs = document.querySelectorAll('input[type="text"]');
-// var btn = document.querySelector("#btn");
-// var h4 = document.querySelector("h4");
-
-// btn.addEventListener("click", function (ele) {
-//     ele.preventDefault();
-//     for(var i = 0; i <inputs.length; i++){
-//        if(inputs[i].value.trim() === ""){
-//         h4.textContent = "Please fill all the fields";
-//         h4.style.color = "red";
-//         break
-//     }
-//     else{
-//         h4.textContent = "Form Submitted";
-//         h4.style.color = "green";
-//     }
-//     }
-
-//
-
-let inp = document.querySelector("input");
-let add = document.querySelector("#add");
-let remove = document.querySelector("#remove");
-let ul = document.querySelector("ul");
-let li = document.createElement("li");
-
-add.addEventListener("click", function () {
-  if (inp.value.trim() == "") {
-    alert("pls Enter the vaue ");
-  } else {
-    li =document.createElement("li")
-    li.textContent = inp.value;
-    ul.appendChild(li);
-    inp.value = "";
-  }
-});
-
-remove.addEventListener("click",function(){
-    ul.removeChild(li)
-})
+document.addEventListener("DOMContentLoaded", function () {
+    let start = document.querySelector("#start");
+    let stop = document.querySelector("#stop");
+    let h3 = document.querySelector("h3");
+    let count;
+    var clr;
+  
+    start.addEventListener("click", function () {
+      count = 0;
+      clr = setInterval(function () {
+        h3.textContent = count;
+        count++;
+      }, 1000);
+    });
+  
+    stop.addEventListener("click", function () {
+      clearInterval(clr);
+    });
+  });
+  
