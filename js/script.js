0a165f4964d7415b6b7b315f06f6db6c4c317d9b
Basuraj <basurajp135@gmail.com>
2023-11-17 17:59:33 +0530

    form valdation usng form done

--- js/script.js
+++ js/script.js
@@ -5,16 +5,36 @@
 //   h.textContent = "Hello World!";
 // });
 
-var img1 = document.querySelector("#img1");
-var img2 = document.querySelector("#img2");
+// var img1 = document.querySelector("#img1");
+// var img2 = document.querySelector("#img2");
 
-var btn = document.querySelector("button");
+// var btn = document.querySelector("button");
 
-btn.addEventListener("click", function () {
-    var img1scr = img1.src
-    var img2scr = img2.src
+// btn.addEventListener("click", function () {
+//     var img1scr = img1.src
+//     var img2scr = img2.src
 
-    img1.src = img2scr
-    img2.src = img1scr
-    console.log('basuraj');
-})
+//     img1.src = img2scr
+//     img2.src = img1scr
+//     console.log('basuraj');
+// })
+
+var inputs = document.querySelectorAll('input[type="text"]');
+var btn = document.querySelector("#btn");
+var h4 = document.querySelector("h4");
+
+btn.addEventListener("click", function (ele) {
+    ele.preventDefault();
+    for(var i = 0; i <inputs.length; i++){
+       if(inputs[i].value.trim() === ""){
+        h4.textContent = "Please fill all the fields";
+        h4.style.color = "red";
+        break
+    }
+    else{
+        h4.textContent = "Form Submitted";
+        h4.style.color = "green";
+    }
+    }
+    
+});
