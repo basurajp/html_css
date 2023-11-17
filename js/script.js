d7865322167a94583592c8e38a2ceafc2b31b378
Basuraj <basurajp135@gmail.com>
2023-11-17 22:29:19 +0530

    add and remove using Dom dom done

--- js/script.js
+++ js/script.js
@@ -19,22 +19,43 @@
 //     console.log('basuraj');
 // })
 
-var inputs = document.querySelectorAll('input[type="text"]');
-var btn = document.querySelector("#btn");
-var h4 = document.querySelector("h4");
+// var inputs = document.querySelectorAll('input[type="text"]');
+// var btn = document.querySelector("#btn");
+// var h4 = document.querySelector("h4");
 
-btn.addEventListener("click", function (ele) {
-    ele.preventDefault();
-    for(var i = 0; i <inputs.length; i++){
-       if(inputs[i].value.trim() === ""){
-        h4.textContent = "Please fill all the fields";
-        h4.style.color = "red";
-        break
-    }
-    else{
-        h4.textContent = "Form Submitted";
-        h4.style.color = "green";
-    }
-    }
-    
+// btn.addEventListener("click", function (ele) {
+//     ele.preventDefault();
+//     for(var i = 0; i <inputs.length; i++){
+//        if(inputs[i].value.trim() === ""){
+//         h4.textContent = "Please fill all the fields";
+//         h4.style.color = "red";
+//         break
+//     }
+//     else{
+//         h4.textContent = "Form Submitted";
+//         h4.style.color = "green";
+//     }
+//     }
+
+//
+
+let inp = document.querySelector("input");
+let add = document.querySelector("#add");
+let remove = document.querySelector("#remove");
+let ul = document.querySelector("ul");
+let li = document.createElement("li");
+
+add.addEventListener("click", function () {
+  if (inp.value.trim() == "") {
+    alert("pls Enter the vaue ");
+  } else {
+    li =document.createElement("li")
+    li.textContent = inp.value;
+    ul.appendChild(li);
+    inp.value = "";
+  }
 });
+
+remove.addEventListener("click",function(){
+    ul.removeChild(li)
+})
