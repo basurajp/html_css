24da316f4d218d1c85657396e0f46e6ab8afa9e6
Basuraj <basurajp135@gmail.com>
2023-11-17 14:55:52 +0530

    images swapping using dom done

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
+// var btn = document.querySelector("button");
+// var h = document.querySelector("h1");
+
+// btn.addEventListener("click", function () {
+//   h.textContent = "Hello World!";
+// });
+
+var img1 = document.querySelector("#img1");
+var img2 = document.querySelector("#img2");
+
 var btn = document.querySelector("button");
-var h = document.querySelector("h1");
 
 btn.addEventListener("click", function () {
-  h.textContent = "Hello World!";
-});
+    var img1scr = img1.src
+    var img2scr = img2.src
+
+    img1.src = img2scr
+    img2.src = img1scr
+    console.log('basuraj');
+})
