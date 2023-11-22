c5786325a8d08a3cc4f347292c94d312a69c185f
Basuraj <basurajp135@gmail.com>
2023-11-22 05:43:07 +0530

    using single button perfomed add and remove operation

--- dom_js/script.js
+++ dom_js/script.js
@@ -1,14 +1,23 @@
 let istatus = document.querySelector("h3");
 let add = document.querySelector("#add");
-
-let remove = document.querySelector("#remove");
+let flag = 0;
 
 add.addEventListener("click", function () {
-  istatus.innerHTML = "Friends";
-  istatus.style.color = "green";
+  if (flag == 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    flag = 1;
+    console.log("if dta ");
+    add.innerHTML = "Remove";
+  } else {
+    istatus.innerHTML = "Stanger";
+    istatus.style.color = "red";
+    flag = 0;
+    add.innerHTML = "Add friend";
+  }
 });
 
-remove.addEventListener("click", function () {
-  istatus.innerHTML = "Stanger";
-  istatus.style.color = "red";
-});
+// remove.addEventListener("click", function () {
+//   istatus.innerHTML = "Stanger";
+//   istatus.style.color = "red";
+// });
