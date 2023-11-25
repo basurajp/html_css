820e108427d3337060e5b0252a8d0fa6c575063c
Basuraj <basurajp135@gmail.com>
2023-11-26 00:07:11 +0530

    bubble game done and working fine

--- bubblegame/index.js
+++ bubblegame/index.js
@@ -1,7 +1,47 @@
-var clutter = "";
-for (let i = 1; i <= 152; i++) {
-  let rn = Math.floor(Math.random() * 10);
-  clutter += `<div class="buble">${rn}</div>`;
+var score = 0;
+var timer = 60;
+
+function increaseScore() {
+  score += 10;
+  document.querySelector("#scoreval").textContent = score;
+}
+var hrn = 0;
+function getnewHit() {
+  hrn = Math.floor(Math.random() * 10);
+  document.querySelector("#hitval").textContent = hrn;
 }
 
-document.querySelector(".pbtm").innerHTML = clutter;
+function makebuble() {
+  var clutter = "";
+  for (let i = 1; i <= 152; i++) {
+    let rn = Math.floor(Math.random() * 10);
+    clutter += `<div class="buble">${rn}</div>`;
+  }
+
+  document.querySelector(".pbtm").innerHTML = clutter;
+}
+function runTimer() {
+  var timerint = setInterval(function () {
+    if (timer > 0) {
+      timer--;
+      document.querySelector("#timerVal").textContent = timer;
+    } else {
+        document.querySelector('.pbtm').innerHTML =`<h1>Game Over</h1>`
+      clearInterval(timerint);
+    }
+  }, 1000);
+}
+
+document.querySelector("#pbtm").addEventListener("click", function (dets) {
+  var clickedNum = Number(dets.target.textContent);
+  if (hrn === clickedNum) {
+    increaseScore();
+    getnewHit();
+    makebuble();
+  }
+});
+
+makebuble();
+runTimer();
+getnewHit();
+// increaseScore();
