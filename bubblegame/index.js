023bd3b7ba093564b11e0ee926d80df5b5b3a58d
Basuraj <basurajp135@gmail.com>
2023-11-26 02:30:19 +0530

    tried own

--- bubblegame/index.js
+++ bubblegame/index.js
@@ -1,47 +1,56 @@
-var score = 0;
-var timer = 60;
+let btm = document.querySelector(".btm");
+let hit = document.querySelector("#hit");
+let btimer = document.querySelector("#btimer");
+let bpanel = document.querySelector("#bpanel");
+let domscore = document.querySelector("#domscore");
 
-function increaseScore() {
-  score += 10;
-  document.querySelector("#scoreval").textContent = score;
-}
-var hrn = 0;
-function getnewHit() {
-  hrn = Math.floor(Math.random() * 10);
-  document.querySelector("#hitval").textContent = hrn;
-}
+let hrn;
+let inttimer = 60;
+let clrTimeInterval;
+let rn;
+let clutter = "";
+let uscore = 0;
+let clicked;
 
-function makebuble() {
-  var clutter = "";
-  for (let i = 1; i <= 152; i++) {
-    let rn = Math.floor(Math.random() * 10);
+function makeBuble() {
+  for (i = 0; i < 102; i++) {
+    rn = Math.floor(Math.random() * 10);
     clutter += `<div class="buble">${rn}</div>`;
+    btm.innerHTML = clutter;
   }
-
-  document.querySelector(".pbtm").innerHTML = clutter;
-}
-function runTimer() {
-  var timerint = setInterval(function () {
-    if (timer > 0) {
-      timer--;
-      document.querySelector("#timerVal").textContent = timer;
-    } else {
-        document.querySelector('.pbtm').innerHTML =`<h1>Game Over</h1>`
-      clearInterval(timerint);
-    }
-  }, 1000);
 }
 
-document.querySelector("#pbtm").addEventListener("click", function (dets) {
-  var clickedNum = Number(dets.target.textContent);
-  if (hrn === clickedNum) {
-    increaseScore();
-    getnewHit();
-    makebuble();
+function makeHit() {
+  hrn = Math.floor(Math.random() * 10);
+  hit.textContent = hrn;
+}
+
+function timer() {
+  if (inttimer < 0) {
+    clrTimeInterval = setInterval(function () {
+      inttimer--;
+      btimer.textContent = inttimer;
+    }, 1000);
+  } else {
+    clearInterval(clrTimeInterval);
+    
+  }
+}
+
+function score() {
+  uscore += 10;
+  domscore.textContent = uscore;
+}
+
+bpanel.addEventListener("click", function (dts) {
+  clicked = Number(dts.target.innerHTML);
+  if (clicked === hrn) {
+    score();
+    makeBuble();
+    makeHit();
   }
 });
 
-makebuble();
-runTimer();
-getnewHit();
-// increaseScore();
+timer();
+makeBuble();
+makeHit();
