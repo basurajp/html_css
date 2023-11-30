3c7e8e28c8eb8e1a427de3252a74a29e562e496c
Basuraj <basurajp135@gmail.com>
2023-12-01 00:39:12 +0530

    quiz  app done

--- quiz/script.js
+++ quiz/script.js
@@ -1,59 +1,101 @@
 // defining the quiz data
-const quizData = [
-    {
-      question: "What is the correct way to declare a JavaScript variable?",
-      options: ["variable x;", "var x;", "x = var;", "declare x;"],
-      correct: 1,
-    },
-    {
-      question: "Which of the following is a JavaScript data type?",
-      options: ["float", "boolean", "string[]", "void"],
-      correct: 1,
-    },
-    {
-      question: "What is the purpose of the `parseInt()` function in JavaScript?",
-      options: [
-        "Parse a string as a floating-point number",
-        "Parse a string as an integer",
-        "Convert a number to a string",
-        "Round a number to the nearest integer",
-      ],
-      correct: 1,
-    },
-    {
-      question: "What does the `===` operator do in JavaScript?",
-      options: [
-        "Checks for equality without type coercion",
-        "Assigns a value to a variable",
-        "Compares values with type coercion",
-        "Checks for equality with type coercion",
-      ],
-      correct: 0,
-    },
-    {
-      question: "Which keyword is used to declare a function in JavaScript?",
-      options: ["function", "method", "func", "define"],
-      correct: 0,
-    },
-  ];
-  
-  let radioButtons = document.querySelectorAll("input[type='radio']");
-  let [questionElem, option1, option2, option3, option4] =
-    document.querySelectorAll("#question, #option1, #option2, #option3, #option4");
-  
-  let btn = document.querySelector("#btn");
-  
-  let currentQuiz = 0;
-  let score = 0;
-  
-  let loadQuiz = () => {
-    let { question, options } = quizData[currentQuiz];
-    questionElem.innerText = question;
-    option1.innerText = options[0];
-    option2.innerText = options[1];
-    option3.innerText = options[2];
-    option4.innerText = options[3];
-  };
-  
-  loadQuiz();
-  
+const quizDB = [
+  {
+    question: "What is the capital of France?",
+    a: "Berlin",
+    b: "Madrid",
+    c: "Paris",
+    d: "Rome",
+    ans: "ans3",
+  },
+  {
+    question: "Which planet is known as the Red Planet?",
+    a: "Mars",
+    b: "Venus",
+    c: "Jupiter",
+    d: "Saturn",
+    ans: "ans1",
+  },
+  {
+    question: 'Who wrote "Romeo and Juliet"?',
+    a: "Charles Dickens",
+    b: "Jane Austen",
+    c: "William Shakespeare",
+    d: "Emily Brontë",
+    ans: "ans3",
+  },
+  {
+    question: "What is the largest mammal on Earth?",
+    a: "Elephant",
+    b: "Blue Whale",
+    c: "Giraffe",
+    d: "Hippopotamus",
+    ans: "ans2",
+  },
+  {
+    question: "In which year did the Titanic sink?",
+    a: "1912",
+    b: "1905",
+    c: "1920",
+    d: "1935",
+    ans: "ans1",
+  },
+];
+
+let question = document.querySelector(".question");
+let opt1 = document.querySelector("#opt1");
+let opt2 = document.querySelector("#opt2");
+let opt3 = document.querySelector("#opt3");
+let opt4 = document.querySelector("#opt4");
+let submit = document.querySelector("#submit");
+let ans = document.querySelectorAll(".ans");
+let showScore = document.querySelector("#showScore");
+
+let questionCount = 0;
+let score = 0;
+let loadQuestion = () => {
+  let questionList = quizDB[questionCount];
+  question.innerHTML = questionList.question;
+  opt1.innerHTML = questionList.a;
+  opt2.innerHTML = questionList.b;
+  opt3.innerHTML = questionList.c;
+  opt4.innerHTML = questionList.d;
+};
+
+loadQuestion();
+
+let getCheckAnswer = () => {
+  let answer;
+  ans.forEach((currentElem) => {
+    if (currentElem.checked) {
+      answer = currentElem.id;
+    }
+  });
+  return answer;
+};
+
+let deselectAll = () => {
+  ans.forEach((currentElem) => (currentElem.checked = false));
+};
+
+submit.addEventListener("click", () => {
+  let checkdAnswer = getCheckAnswer();
+  console.log(checkdAnswer);
+
+  if (checkdAnswer === quizDB[questionCount].ans) {
+    score++;
+  }
+  questionCount++;
+
+  deselectAll();
+
+  if (questionCount < quizDB.length) {
+    loadQuestion();
+  } else {
+    showScore.innerHTML = `
+<h3>You Score ${score}/${quizDB.length}</h3>
+<button class="btn" onclick="location.reload()"> Play again </button>  
+    `;
+    showScore.classList.remove("scoreArea");
+  }
+});
---
+++ scroll _trigger/app.js
@@ -0,0 +1,7 @@
+gsap.from('.circle',{
+    scale:0,
+    delay:1,
+    duration:2,
+    rotate:720
+scrollTrigger:
+})
