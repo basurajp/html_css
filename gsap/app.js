3f6e57aa4724d247b85cc1a89e5cf9a704378136
Basuraj <basurajp135@gmail.com>
2023-12-04 06:36:29 +0530

    scrol trigger started

--- gsap/app.js
+++ gsap/app.js
@@ -1,23 +1,17 @@
-var tl = gsap.timeline();
-tl.from("h4", {
-  y: -50,
-  duration: 1,
-  delay: 0.3,
-  opacity: 0,
-  stagger: 0.3,
+gsap.from(".page1 .circle", {
+  scale: 0,
+  delay: 1,
+  duration: 2,
+  rotate: 360,
 });
+gsap.from(".page2 .circle", {
+  scale: 0,
 
-tl.from("h1", {
-  x: -500,
-  opacity: 0,
-  duration: 0.8,
-  stagger: 0.2,
-});
-
-tl.from("img", {
-  x: 100,
-  rotate: 45,
-  opacity: 0.5,
-  duration: 0.5,
-  stagger: 0.5,
+  duration: 2,
+  rotate: 360,
+  scrollTrigger: {
+    trigger: ".page2 .circle",
+    scroller: 'body',
+    markers:true
+  },
 });
