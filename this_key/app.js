b7c7e4e9aa404499700ba678d143144cc28412be
Basuraj <basurajp135@gmail.com>
2023-12-12 02:44:29 +0530

    q 5  complted

--- this_key/app.js
+++ this_key/app.js
@@ -1,15 +1,7 @@
-// Problem 4: (Medium) Write a JavaScript program that takes two arguments for a Book constructor: title and author. Use the this keyword to assign these parameters. Create a method called detail() which returns a string that includes both the book title and its author.
-
-
-function Book (title,author){
-  this.title = title ;
-  this.author = author ;
-  this.detail = function(){
-    return `Title is : ${this.title} and Author is : ${this.author}`
+// Create a nested object 'nestedObj' where an inner object 'innerObj' has a method 'print'. Inside 'print', log 'this' keyword and interpret the output.
+var nestedObj = {
+  innerObj: {
+    print: function() { console.log(this); }
   }
-
 }
-
-let book1  = new Book('Rich dad poor dad ', 'Robert')
-
-console.log(book1.detail());
+nestedObj.innerObj.print();
